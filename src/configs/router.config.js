88f14c245f81c27fcccba38898d494dc4ae5a56f
BETGENIUS\juan.isaza <juan.isaza@geniussportsmedia.com>
2018-03-21 09:56:53 -0500

    adding headers

--- src/configs/router.config.js
+++ src/configs/router.config.js
@@ -1,13 +1,5 @@
 import {pushStateLocationPlugin} from '@uirouter/react';
 import {hello, world} from "../states";
-import * as axios from "axios/index";
-
-axios.interceptors.request.use(function (config) {
-
-    return config;
-}, function (error) {
-    return Promise.reject(error);
-});
 
 export const plugins = [
     pushStateLocationPlugin
